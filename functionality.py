50b9823061af87b6166535dce3ae0cb4fa5bf0e1
eliaw <elia.waefler@gmail.com>
2025-01-13 09:42:53 +0100

    test

--- functionality.py
+++ functionality.py
@@ -11,3 +11,26 @@ def extract_file_structure(uploaded_file):
         df = excel_file.parse(sheet)
         structure[sheet] = list(df.columns)
     return structure
+
+
+
+def standard_tabelle():
+
+    # A     B       C       D       E           F               G           H           ...
+    # ID    GUID    name    wert    Einheit     dim1            dim1        dim3        ...
+    # 01    fokfj   p1      5       h           G2              19.01.2025  projekt A   ...
+
+    # Zeiterfassung
+    # 01    jdskl   Elia    5       h           zeiterf         09.01.2025  Projekt1
+
+    # energiedashboard
+    # 01    sdflkj          5       h           Strom           01.2025
+
+    # tabellenname
+
+    pass
+
+
+
+def finde_tabelle():
+    pass
