aa32d6ce9710322187b5bdb0defc945aedd9dfa8
eliaw <elia.waefler@gmail.com>
2025-01-13 17:35:32 +0100

    app

--- anwendung.py
+++ anwendung.py
@@ -29,20 +29,28 @@ def main():
         st.session_state.counter = False
     if "create_mapper" not in st.session_state:
         st.session_state.create_mapper = False
+
+
     st.write("Excel File Mapper")
+
+    st.write("")
+    st.write("")
+    st.write("")
     # Step 1: Upload QUELLE and ZIEL files
-    st.write("")
-    st.write("")
-    st.write("")
     st.write("Step 1: Upload Excel Files")
     c1, c2, c3 = st.columns(3)
     with c1:
         uploaded_quelle = st.file_uploader("Upload QUELLE Excel File", type="xlsx", key="quelle")
     with c2:
-        mapper_table = st.file_uploader("upload mapper table", type="csv", key="mapper_table")
+        uploaded_mapping_table = st.file_uploader("upload mapper table", type="csv", key="mapping_table")
     with c3:
         uploaded_ziel = st.file_uploader("Upload ZIEL Excel File", type="xlsx", key="ziel")
 
+    st.write("")
+    st.write("")
+    st.write("")
+    # create Mapping
+    st.write("Step 2: Mapping")
     if st.button("New Mapping Table"):
         if uploaded_quelle and uploaded_ziel:
             st.session_state.create_mapper = True
@@ -58,39 +66,42 @@ def main():
                 with r:
                     st.write(rule_infos[rule_n])
 
-        st.write("Select Mappings")
+        st.write("CREATE Mappings")
         quelle_structure = extract_file_structure(uploaded_quelle)
         ziel_structure = extract_file_structure(uploaded_ziel)
 
         # Display QUELLE structure on the left and ZIEL mapping options on the right
         mapping_table = []
         for sheet, columns in quelle_structure.items():
-            my_columns = list(columns)
             st.write(f"Mapping for Sheet: {sheet}")
-            add_col = st.selectbox(
-                f"Additional Quelle Column",
-                columns,
-                key=f"add_quelle_{len(my_columns)}"
-            )
-            if st.button("Add Column"):
-                my_columns.append(add_col)
-            for column_nb in range(len(my_columns)):
-                col1, col2, col3 = st.columns(3)
+            st.session_state.my_columns = []
 
+            add_c1, add_c2, add_c3, _ = st.columns(4)
+            with add_c1:
+                if st.button("Add Column"):
+                    st.session_state.my_columns.append(f"{st.session_state.add_col}_{len(st.session_state.my_columns)}")
+            with add_c2:
+                st.session_state.add_col = st.selectbox(
+                    f"Additional Quelle Column",
+                    columns,
+                    key=f"add_quelle_{len(st.session_state.my_columns)}"
+                )
+            for column in list(columns + st.session_state.my_columns):
+                col1, col2, col3 = st.columns(3)
                 with col1:
-                    st.text(f"QUELLE Column: {my_columns[column_nb]}")
+                    st.text(f"QUELLE Column: {column}")
                 with col2:
                     l, r = st.columns(2)
                     with l:
-                        rule = st.selectbox(
+                        my_rule = st.selectbox(
                             f"Select Rule",
                             rules,
-                            key=f"rule_{sheet}_{column_nb}"
+                            key=f"rule_{sheet}_{column}"
                         )
                     with r:
-                        rule_param = st.text_input(
+                        my_rule_param = st.text_input(
                             f"Rule Param",
-                            key=f"rule_param_{sheet}_{column_nb}"
+                            key=f"rule_param_{sheet}_{column}"
                         )
                 with col3:
                     l, r = st.columns(2)
@@ -98,24 +109,23 @@ def main():
                         ziel_sheet = st.selectbox(
                             f"Select ZIEL Sheet",
                             list(ziel_structure.keys()),
-                            key=f"Transformation_Rule_{sheet}_{column_nb}"
+                            key=f"Transformation_Rule_{sheet}_{column}"
                         )
                     with r:
-                        ziel_column = st.selectbox(
+                        my_ziel_column = st.selectbox(
                             f"Select ZIEL Column",
                             ziel_structure[ziel_sheet],
-                            key=f"ziel_column_{sheet}_{column_nb}"
+                            key=f"ziel_column_{sheet}_{column}"
                         )
-
                 mapping_table.append({
                     "Quelle_File": uploaded_quelle.name,
                     "Quelle_Sheet": sheet,
-                    "Quelle_Column": my_columns[column_nb],
-                    "Transformation_Rule": rule,
-                    "Transformation_Rule_param": rule_param,
+                    "Quelle_Column": column,
+                    "Transformation_Rule": my_rule,
+                    "Transformation_Rule_param": my_rule_param,
                     "Ziel_File": uploaded_ziel.name,
                     "Ziel_Sheet": ziel_sheet,
-                    "Ziel_Column": ziel_column,
+                    "Ziel_Column": my_ziel_column,
                 })
 
         # Display the mapping table
@@ -135,19 +145,20 @@ def main():
                 key="download_csv"
             )
 
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("Step 3: execute")
     # Execute transformation
-    st.write("")
-    st.write("")
-    st.write("")
-    if st.button("Execute Transformation"):
+    if st.button("Execute Transformation") and uploaded_mapping_table:
         if not st.session_state.mapper:
             st.error("select or create mapping table")
         else:
             ziel_preview = []
 
-            for mapping in mapping_table:
-                quelle_df = pd.read_excel(uploaded_quelle, sheet_name=mapping["Quelle_Sheet"])
-            ziel_df = pd.read_excel(uploaded_ziel, sheet_name=mapping["Ziel_Sheet"])
+            for mapping in uploaded_mapping_table:
+                quelle_df = pd.read_excel(uploaded_quelle, sheet_name=mapping[0])
+            ziel_df = pd.read_excel(uploaded_ziel, sheet_name=mapping[6])
 
             quelle_column = mapping["Quelle_Column"]
             ziel_column = mapping["Ziel_Column"]
